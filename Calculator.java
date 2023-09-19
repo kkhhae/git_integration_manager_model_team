e8664eecc8bd87fa40711f3138e448ea0d7d002e
kkhhae <rlagodh3662@gmail.com>
2023-09-19 17:09:26 +0900

    branch

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,18 @@
 public class Calculator {
     public static void main(String[] args) {
+
+	System.out.println("간단한 콘솔 계산기 프로그램");
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("사용할 연산자를 입력하세요 (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+
 }
 }
